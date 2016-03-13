299e52f2fc9c0ce1b5e9acf5e228823c4e8f5b13
jacysun <jacysun@users.noreply.github.com>
2016-03-13 12:05:09 -0400

    Update Reagent.java

--- src/user/Reagent.java
+++ src/user/Reagent.java
@@ -1,5 +1,45 @@
 package user;
 
 public class Reagent {
+  
+  private String rname;
+  private String source;
+  private String type;
+  private String expireDate;
+  private int scoreNum;
+  private double avrscore;
+  
+  public Reagent(String rname, String source, String type, String expireDate, int scoreNum, double avrscore) {
+    this.rname = rname;
+    this.source = source;
+    this.type = type;
+    this.expireDate = expireDate;
+    this.scoreNum = scoreNum;
+    this.avrscore = avrscore;
+  }
+  
+  public String getRname() {
+    return rname;
+  }
+  
+  public String getSource() {
+    return source;
+  }
+  
+  public String getType() {
+    return type;
+  }
+  
+  public String expireDate() {
+    return expireDate;
+  }
+  
+  public int getScoreNum() {
+    return scoreNum;
+  }
+  
+  public double getAvrscore() {
+    return avrscore;
+  }
 
 }
